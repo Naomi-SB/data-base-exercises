a799bf924345934824a00fab1a2151e9bf32cde5
Naomi Sweatt <sweatt.b.naomi@gmail.com>
2022-09-08 14:34:46 -0500

    order by exercises complete. previous prompts not copied

---
+++ order_by_exercises.sql
@@ -0,0 +1,19 @@
+1. New file created, contents not copied. 
+
+2. What was the first and last name in the first row of the results? What was the first and last name of the last person in the table?
+--- Irena Reutenauer is the first name on the list. Vidya Simmen is the lat name on the list.
+
+3. What was the first and last name in the first row of the results? What was the first and last name of the last person in the table? 
+--- The name on the top of the list is Irena Acton. The name of the last person is Vidya Zweizig.
+
+4. What was the first and last name in the first row of the results? What was the first and last name of the last person in the table?
+---The first person on the list is Irena Acton. The last person on the list is Maya Zyda.
+
+5. Enter a comment with the number of employees returned, the first employee number and their first and last name, and the last employee number with their first and last name.
+-- Number of employees returned: 899. First employee number: Ramzi Erde 10021. Last employee number: Tadahiro Erde 499648.
+
+6. Enter a comment with the number of employees returned, the name of the newest employee, and the name of the oldest employee.
+--- 899 employees returned. The newest hire is Teiji Eldridge. The oldest hire is Sergi Erde.
+
+7. Find all current or previous employees employees whose last name starts and ends with 'E'. Enter a comment with the number of employees whose last name starts and ends with E. How many employees last names end with E, regardless of whether they start with E?
+ --- 362 employees returned. Of the oldest employees, Khun Bernini was hired last. Of the youngest employees, Douadi Pettis was hired first. 
