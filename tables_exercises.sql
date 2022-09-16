a742c09ce7b95e11b80bfc7b110f06fae7193c1b
Naomi Sweatt <sweatt.b.naomi@gmail.com>
2022-09-15 22:14:52 -0500

    updated for turn in

--- tables_exercises.sql
+++ tables_exercises.sql
@@ -1,24 +1,31 @@
-3. Use the employees database. Write the SQL code necessary to do this.
-#USE employees;
-4 . List all the tables in the database. Write the SQL code necessary to accomplish this.
-# departments, dep_emp, dep_manager, employees, salaries, titles
-# SHOW tables;
-5. Explore the employees table. What different data types are present on this table?
-# int, date, varchar, enum are the types
-6. Which table(s) do you think contain a numeric type column? (Write this question and your answer in a comment)
-# I think that the salaries table will include numeric types
+--1. Open MySQL Workbench and login to the database server
+--2. Save your work in a file named tables_exercises.sql
 
-7. Which table(s) do you think contain a string type column? (Write this question and your answer in a comment)
-# I think that the titles table will include string type column
 
-8. Which table(s) do you think contain a date type column? (Write this question and your answer in a comment)
-# I think the employee table will include date types. 
 
-9. What is the relationship between the employees and the departments tables? (Write this question and your answer in a comment)
-# There is no relationship between the two tables other than that they are in the same data base
-#
-10. Show the SQL that created the dept_manager table. Write the SQL it takes to show this as your exercise solution.
-# CREATE TABLE `dept_manager` (
+--3. Use the employees database. Write the SQL code necessary to do this.
+  USE employees;
+
+--4 . List all the tables in the database. Write the SQL code necessary to accomplish this.
+  SHOW TABLES;
+  
+  --5. Explore the employees table. What different data types are present on this table?
+  # int, date, varchar, enum are the types
+
+--6. Which table(s) do you think contain a numeric type column? (Write this question and your answer in a comment)
+  # I think that the salaries table will include numeric types
+
+--7. Which table(s) do you think contain a string type column? (Write this question and your answer in a comment)
+  # I think that the titles table will include string type column
+
+--8. Which table(s) do you think contain a date type column? (Write this question and your answer in a comment)
+  # I think the employee table will include date types. 
+
+--9. What is the relationship between the employees and the departments tables? (Write this question and your answer in a comment)
+  # There is no relationship between the two tables other than that they are in the same data base
+
+--10. Show the SQL that created the dept_manager table. Write the SQL it takes to show this as your exercise solution.
+  # CREATE TABLE `dept_manager` (
   `emp_no` int NOT NULL,
   `dept_no` char(4) NOT NULL,
   `from_date` date NOT NULL,
@@ -28,4 +35,5 @@
   CONSTRAINT `dept_manager_ibfk_1` FOREIGN KEY (`emp_no`) REFERENCES `employees` (`emp_no`) ON DELETE CASCADE ON UPDATE RESTRICT,
   CONSTRAINT `dept_manager_ibfk_2` FOREIGN KEY (`dept_no`) REFERENCES `departments` (`dept_no`) ON DELETE CASCADE ON UPDATE RESTRICT
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1
-# SHOW CREATE TABLE dept_manager;
+
+  --# SHOW CREATE TABLE dept_manager;
